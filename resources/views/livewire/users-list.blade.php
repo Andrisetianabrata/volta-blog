e69ee8cfd55bea2b564157a96ae1ac20bc2d2f56
Andri Setianabrata <andri.setiana.brata69@gmail.com>
2024-03-13 02:25:52 +0700

    add badage Blocked

--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -29,10 +29,18 @@
             @forelse ($users as $user)
             <div class="col-md-6 col-lg-3">
                <div class="card">
-                  <div class="card-body p-4 text-center">
+                  @if ($user->blocked == 1)
+                     <div class="ribbon bg-red">Blocked</div>
+                  @endif
+                  @if ($user->authorType->id == 1)
+                     <div class="ribbon bg-green">Admin</div>
+                  @endif
+                  <div class="card-cover card-cover-blurred text-center" style="background-image: url({{$user->picture}})">
                      <img class="avatar avatar-xl mb-3 rounded" src="{{$user->picture}}"></img>
+                  </div>
+                  <div class="card-body p-4 text-center">
                      <h3 class="m-0 mb-1">{{$user->name}}</h3>
-                     <div class="text-muted">@ {{$user->username}}</div>
+                     <div class="text-muted">{{'@'}}{{$user->username}}</div>
                      <div class="mt-3">
                         <span class="badge {{ ($user->authorType->id == 1) ? 'bg-purple-lt' : 'bg-green-lt' }}">{{$user->authorType->name}}</span>
                      </div>
