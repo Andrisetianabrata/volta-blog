05bc3d24c85921bcf728f844c0f832b6286f5018
Andrisetianabrata <andri.setiana.brata69@gmail.com>
2024-03-19 14:14:07 +0700

    ----

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\UserUniqueFilesFolder::class,
-            \App\Http\Middleware\ChekBlockedUser::class,
+            // \App\Http\Middleware\ChekBlockedUser::class,
         ],
 
         'api' => [
